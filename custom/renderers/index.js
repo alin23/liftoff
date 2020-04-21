2ed64666f97c8af963ad2964203ac643fa5a8c9c
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-04-21 17:45:08 +0300

    Stylus and date

--- custom/renderers/index.js
+++ custom/renderers/index.js
@@ -1,7 +1,10 @@
 import Pret from "./Pret";
+import LiftoffHero from "./Hero";
 
 export {
     Pret as PretMinim,
     Pret as PretMaxim
 };
-export default {};
+export default {
+    LiftoffHero
+};
